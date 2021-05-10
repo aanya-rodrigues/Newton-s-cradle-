5d65deab95ecb12ccdf1b9e324570fddbcf98cd1
aanya-rodrigues <79843166+aanya-rodrigues@users.noreply.github.com>
2021-05-10 14:55:19 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,34 @@
+class Bob
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:true,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var pos=this.body.position;		
+
+			push()
+			translate(pos.x, pos.y);
+			rectMode(CENTER)
+			fill("pink");
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
